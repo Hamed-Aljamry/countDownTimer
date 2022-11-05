044b4a1be69445ee8365f13aa2aa724690d14c65
hamed aljamry <hamed970@hotmail.com>
2022-11-05 19:59:26 +0000

    caluclated seconds until endDate

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const newYears = "1 Jan 2023"
+
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const seconds = (newYearsDate - currentDate) / 1000;
+
+  const days = (seconds / 3600 / )
+
+  console.log(newYearsDate - currentDate);
+}
+
+countDown();
+
+
+
+// console.log(3600 / 24)
