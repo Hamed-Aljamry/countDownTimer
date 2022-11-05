44b02fca9a4738dafc6c31956c0ba30e57c647e1
hamed aljamry <hamed970@hotmail.com>
2022-11-05 20:41:35 +0000

    got elements by ID and inserted them into teh function and switched their inner html, added setinterval function, finished all what is needed for this

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const newYears = "1 Jan 2023"
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+
+
+const newYears = "1 Jan 2023";
 
 function countDown() {
   const newYearsDate = new Date(newYears);
@@ -14,11 +20,15 @@ function countDown() {
 
   const seconds = Math.floor(totalSeconds % 60)
 
+daysElement.innerHTML = days;
+hoursElement.innerHTML = hours;
+minutesElement.innerHTML = minutes;
+secondsElement.innerHTML = seconds;
   console.log(days, hours, minutes, seconds);
 }
 
 countDown();
-
-
+// set interval so it could display this function every 1000 miliseconds
+setInterval(countDown, 1000)
 
 // console.log(3600 / 24)
