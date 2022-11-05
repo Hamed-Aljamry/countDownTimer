ba1371f5da8d50132082eaaf7b6c9e57e8be0266
hamed aljamry <hamed970@hotmail.com>
2022-11-05 20:32:40 +0000

    minutes until endDate working, seconds working too

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ function countDown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = (newYearsDate - currentDate) / 1000;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(seconds / 3600 / 24);
+  const days = Math.floor(totalSeconds / 3600 / 24);
 
-  const hours = Math.floor(seconds/ 3600) % 24
+  const hours = Math.floor(totalSeconds/ 3600) % 24;
 
-  const minutes = Math.floor(seconds / 60) % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
 
-  console.log(days, hours, minutes);
+  const seconds = Math.floor(totalSeconds % 60)
+
+  console.log(days, hours, minutes, seconds);
 }
 
 countDown();
